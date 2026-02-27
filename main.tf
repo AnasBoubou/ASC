f9cf064293cb65a55f80bd15180a4874a4fbbab2
AnasBoubou <sanas.boudadi@gmail.com>
2026-02-27 16:47:04 +0100

    add authorized keys

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "openstack_compute_keypair_v2" "main" {
   name = "my-keypair"
 }
 
+variable "ssh_public_keys" {
+  type    = list(string)
+  default = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDzR42jxMW07I/mdGh9lOKRNOHHC+N+Ho+mJ9oC8jnGGcnwdO7/cyayXufXMHkf1wwyFynOliEiDc+tDpBLz4m1Nm7b1xjRMmpdEcy0hlbuZmpC//h4nCQFXfhknO/jEDEd0vtYeUYGNAFXHjELLbrEITl+w6ABeQKd/DWUZHzhpPnwM1/8SqQRGntQQQ4IIrIdCBO1Pkm2EDCrs85XfDdcvExA+SGK4xD0grEHnkAf9+CwILaVBbkOLEAUR47TugkKFotL1TkWcZBRy7w5TFigVRG6cWmi6TkK0Ci9fmCfL9lPU+9Kt5eF+O7PHbsG7SasC1H8MBRMJNZF9lgHYwc/pztMZ+aKh52P334nAwzDX6R8QLkBIkYNlyZHeB3HNQ4aR6tSk2cCZwbCztS+d4bSqkLbXydh4goFueo8tnqNtAaU+Rpe5eojAPH8GMwkVMXA7/mSl+evVxu5+0WaqpH8XZhH/T9MdJp7/PrGRc0Ls5RCcAmC9CyBkE84EbGlT8VFZT9Zo0y2+uHCyoQLg6TxzAE5hf7RwBWQWbhk6MFcm0k7gxhyKIrGhfipIPeg6CtLAirgNRDKJ2jOhkEni32UElkm90H/xVNqFkIdDM0r6PpxIODFqhg8YcAxd7tVf3YzZxeDN0ftkHZtvwnqNDKKxnIvwPL4xUFHQlyJNBgvzw== sanas@AnasB",
+  ]
+}
+
 # Définition de ta machine virtuelle (Instance)
 resource "openstack_compute_instance_v2" "main" {
   name            = "Ahmidou-Anas-v2" # J'ai mis v2 pour forcer la mise à jour
@@ -42,6 +49,16 @@ resource "openstack_compute_instance_v2" "main" {
   network {
     name = "Ext-Net" # Réseau public d'OVH
   }
+  # On injecte toutes les clés dans l'utilisateur par défaut
+  user_data = <<-EOF
+    #cloud-config
+    users:
+      - name: ubuntu
+        sudo: ALL=(ALL) NOPASSWD:ALL
+        shell: /bin/bash
+        ssh_authorized_keys:
+          ${indent(10, join("\n", [for key in var.ssh_public_keys : "- ${key}"]))}
+  EOF
 }
 
 # Affichage de l'IP après la création
