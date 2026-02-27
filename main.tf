a872435ee9bad69bd5c161f1c26363e8d7d27196
bouznika19204 <ahmidouctx@gmail.com>
2026-02-27 10:58:54 +0100

    ADD

--- main.tf
+++ main.tf
@@ -5,26 +5,46 @@ terraform {
       version = "~> 1.54"
     }
   }
+
+  # Configuration du stockage distant (Backend)
+  backend "s3" {
+    bucket   = "ahmidou-anas"
+    key      = "terraform.tfstate"
+    region   = "sbg"
+    endpoint = "s3.sbg.perf.cloud.ovh.net"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
 }
 
+# Configuration du fournisseur OVH
 provider "openstack" {
-  cloud = "ovh-sbg5"
+  auth_url    = "https://auth.cloud.ovh.net/v3"
+  region      = "SBG5" # Vérifie si c'est SBG ou SBG5 selon ton openrc
 }
- 
+
+# Création d'une paire de clés SSH (si tu ne l'as pas déjà fait)
 resource "openstack_compute_keypair_v2" "main" {
-  name       = "my-keypair"
-  public_key = file("~/.ssh/id_rsa.pub")
+  name = "my-keypair"
 }
- 
+
+# Définition de ta machine virtuelle (Instance)
 resource "openstack_compute_instance_v2" "main" {
-  name            = "Ahmidou-VM"
-  image_name      = "Ubuntu 24.04"
-  flavor_name     = "d2-2"
+  name            = "Ahmidou-Anas-v2" # J'ai mis v2 pour forcer la mise à jour
+  image_name      = "Ubuntu 22.04"    # À vérifier selon les images dispo
+  flavor_name     = "d2-2"            # À vérifier selon ton quota (ex: s1-2, d2-2)
   key_pair        = openstack_compute_keypair_v2.main.name
   security_groups = ["default"]
+
+  network {
+    name = "Ext-Net" # Réseau public d'OVH
+  }
 }
 
+# Affichage de l'IP après la création
 output "instance_ip" {
-  description = "IP publique de la VM"
-  value       = openstack_compute_instance_v2.main.access_ip_v4
+  value = openstack_compute_instance_v2.main.access_ip_v4
 }
